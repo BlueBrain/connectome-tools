eb51ba841769c73cd9e3c923b6c5599be9a5fe85
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2017-09-14 16:35:50 +0200

    Introduced 'connectome-stats' script
    Change-Id: Ifbc1a20eecc1afe31348eb2e870538a6d5202fba

--- bluerecipe/setup.py
+++ bluerecipe/setup.py
@@ -10,6 +10,7 @@ setup(
     name="bluerecipe",
     version=__version__,
     install_requires=[
+        'click>=6.7',
         'equation>=1.2',
         'lxml>=3.3',
         'bluepy>=0.11,<0.12',
@@ -24,6 +25,7 @@ setup(
     license="BBP-internal-confidential",
     scripts=[
         'apps/s2f-recipe',
+        'apps/connectome-stats',
     ],
     url="http://bluebrain.epfl.ch",
     include_package_data=True
