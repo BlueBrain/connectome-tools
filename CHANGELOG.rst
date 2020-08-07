ee8ac9c313f00b4fc268a7de0e60feceede378f8
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2020-08-07 11:07:43 +0200

    Fix doc, remove old nix
    Change theme from haiku to sphinx-bluebrain-theme.
    Copy docs structure from cookiecutter.
    Move content of index.rst to README.rst,
    remove :ref: from the internal reference links.
    Add CHANGELOG.rst.
    
    Change-Id: If2f11a2bdea1926d5d811d8559bb1885edf9b350

---
+++ CHANGELOG.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+Version 0.3.3
+-------------
+
+New Features
+~~~~~~~~~~~~
+- Add support for `p_A` and `pMu_A` parameters.
+
+
+Removed Features
+~~~~~~~~~~~~~~~~
+- Drop support for Python 2.7.
+
+
+Improvements
+~~~~~~~~~~~~
+- Add ``--jobs`` option to parallelize tasks.
--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # pylint: disable=missing-docstring
+import sys
 
 from setuptools import find_packages, setup
 
 from connectome_tools.version import __version__
 
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python < 3.6 is not supported")
+
+# read the contents of the README file
+with open("README.rst", encoding="utf-8") as f:
+    README = f.read()
+
 setup(
     name="connectome-tools",
+    author="BlueBrain NSE",
+    author_email="bbp-ou-nse@groupes.epfl.ch",
     version=__version__,
+    description="Connectome statistics; S2F recipe generation",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://bbpteam.epfl.ch/documentation/projects/connectome-tools",
+    project_urls={
+        "Tracker": "https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
+        "Source": "ssh://bbpcode.epfl.ch/nse/connectome-tools",
+    },
+    license="BBP-internal-confidential",
     install_requires=[
         "click>=7.0,<8.0",
         "equation>=1.2",
@@ -21,14 +40,20 @@ setup(
         "voxcell>=2.5.6",
     ],
     packages=find_packages(),
-    author="BlueBrain NSE",
-    author_email="bbp-ou-nse@groupes.epfl.ch",
-    description="Connectome statistics; S2F recipe generation",
-    license="BBP-internal-confidential",
+    python_requires=">=3.6",
+    extras_require={"docs": ["sphinx", "sphinx-bluebrain-theme"]},
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
     scripts=[
         "apps/connectome-stats",
         "apps/s2f-recipe",
     ],
-    url="https://bbpteam.epfl.ch/project/issues/projects/NSETM/issues",
-    download_url="ssh://bbpcode.epfl.ch/nse/connectome-tools",
 )
