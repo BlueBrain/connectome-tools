b6dad3362b190feb1f4726ecec4de91c3920a078
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2022-01-26 09:53:17 +0000

    [NSETM-1749] Set basepython to 3.9

--- connectome_tools/version.py
+++ connectome_tools/version.py
@@ -1,3 +1,3 @@
 """Package version."""
-VERSION = "0.6.2"
+VERSION = "0.6.3.dev0"
 __version__ = VERSION
--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     entry_points={
